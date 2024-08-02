d4801bb87849455acf12aac44f4ea636e15a31a8
Pepe <you@example.com>
2024-08-02 12:06:35 -0600

    FitText mejoras. Botones, iconos de Internet Services

--- src/itFitText.js
+++ src/itFitText.js
@@ -1,41 +1,4 @@
 /**
- * Fits text within a given HTML element's width, reducing font size by 0.5px down to minFotSizePx
- * TIP: on multiple calls reset font-size before each call.
- *
- * @param {string|HTMLElement|jQuery} element - The ID of the element or the actual element object, or jQuery.
- * @param {number} [minFontSizePx=9] - The minimum font size in pixels. Defaults to 8px.
- * @returns {boolean} Returns true if the text fits or was reduced to fit, false otherwise.
- */
-function itFitText(element, minFontSizePx = 10) {
-
-    try {
-        var el
-        if(typeof element === 'string')
-            el = document.getElementById(element);
-        else
-            el = typeof jQuery === 'function' && element instanceof jQuery ? element[0] : element;
-        if(typeof el === 'undefined' || el === null || typeof el !== 'object')
-            return false;
-        if(el.scrollWidth <= el.clientWidth)
-            return true;
-        if(isNaN(minFontSizePx) || minFontSizePx < 1)
-            minFontSizePx = 10;
-
-        var maxLoops = 16;
-        var computedFontSize = window.getComputedStyle(el, null).getPropertyValue('font-size');
-        if(computedFontSize === null || typeof computedFontSize !== 'string' || computedFontSize === '')
-            return false;
-        var fontSize = parseFloat(computedFontSize.replace('px', ''));
-        while(el.scrollWidth > el.clientWidth && fontSize > minFontSizePx && --maxLoops > 0)
-            el.style.fontSize = (fontSize -= 0.5) + 'px';
-        return true;
-    } catch(e) {
-        console.log("Error itFitText", e);
-        return false;
-    }
-}
-
-/**
  * Fits text within a given HTML element's width, reducing font size by 0.5px down to minFotSizePx to all elements matching the given selector or elements
  * TIP: on multiple calls reset font-size before each call.
  *
@@ -63,7 +26,7 @@ function itFitTextAll(selector, minFontSizePx = 10) {
             return;
         }
         for(var i = 0, len = elements.length; i < len; ++i)
-            _fitTheText2Element(elements[i]);
+            _calculateFontSize2Element(elements[i]);
         function _fitTheText2Element(el) {
             try {
                 var maxLoops = 16;
@@ -71,13 +34,41 @@ function itFitTextAll(selector, minFontSizePx = 10) {
                 if(computedFontSize === null || typeof computedFontSize !== 'string' || computedFontSize === '')
                     return;
                 var fontSize = parseFloat(computedFontSize.replace('px', ''));
+                var fontSizeIni = fontSize;
                 while(el.scrollWidth > el.clientWidth && fontSize > minFontSizePx && --maxLoops > 0)
                     el.style.fontSize = (fontSize -= 0.5) + 'px';
             } catch(err) {
-                js_errores_a_dime(e);
+                if(typeof js_errores_a_dime === 'function')
+                    js_errores_a_dime(err);
+                el.style.fontSize = fontSizeIni + 'px';
+            }
+        }
+        function _calculateFontSize2Element(el) {
+            try {
+                var maxLoops = 16;
+                var computedFontSize = window.getComputedStyle(el, null).getPropertyValue('font-size');
+                if (computedFontSize === null || typeof computedFontSize !== 'string' || computedFontSize === '')
+                    return;
+                var fontSize = parseFloat(computedFontSize.replace('px', ''));
+                if(fontSize <= minFontSizePx || el.scrollWidth <= el.clientWidth)
+                    return;
+                var fontSizeIni = fontSize;
+                var desiredWidth = el.scrollWidth;
+                var availableWidth = el.clientWidth;
+                var scaleFactor = el.clientWidth / el.scrollWidth;
+                var newFontSize = fontSize * scaleFactor;
+                while (el.scrollWidth > el.clientWidth && newFontSize > minFontSizePx && --maxLoops > 0) {
+                    el.style.fontSize = newFontSize + 'px';
+                    newFontSize -= 0.5;
+                }
+            } catch (err) {
+                if (typeof js_errores_a_dime === 'function')
+                    js_errores_a_dime(err);
+                el.style.fontSize = fontSizeIni + 'px';
             }
         }
     } catch (e) {
-        js_errores_a_dime(e)
+        if(typeof js_errores_a_dime === 'function')
+            js_errores_a_dime(e)
     }
 }
